9fe53fd211d669d863744329768fd73fc90e79c4
Alexey Puchkov <alexey@oktopost.com>
2017-03-21 18:20:26 +0200

    Add prototype for event

---
+++ src/event.js
@@ -0,0 +1,20 @@
+"use strict";
+
+
+var is = require('oktopost-plankton').is;
+var func = require('oktopost-plankton').func;
+
+
+function Event(name) {
+	
+	function event() {
+		
+	}
+	
+	
+	
+	return 
+}
+
+
+module.exports = Event;
